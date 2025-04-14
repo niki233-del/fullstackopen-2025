79a4121965569ae198bcbc5a0dd22bf3a7423088
niki233-del <bmandhive@gmail.com>
2025-04-14 22:13:33 +0530

    Delete Total.js

--- Total.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Total = ({ parts }) => {
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
-  return <p>Number of exercises {total}</p>
-}
-
-export default Total
